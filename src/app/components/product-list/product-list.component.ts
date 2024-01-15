298b27ab9966480a9fcb1ef8b6d79d60e333f3c6
DmitriyKovchugo <dkovchugo@mail.ru>
2024-01-15 14:17:48 +0300

    feat: get courses

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -6,13 +6,18 @@ import { IProduct } from '../../services/products.service';
   standalone: true,
   imports: [],
   template: `
-  <h2>{{ title }}</h2>,
-  <h4>{{ subtitle }}</h4>,
+  <h2>{{ title }}</h2>
+  <h4>{{ subtitle }}</h4>
   <ul class="products">
     @for (product of products; track product.id) {
       <li class="product-item">
         <div class="product-image">
           <img [src]="product.image" [alt]="product.title">
+        </div>
+        <div class="product-info">
+          <h3>{{ product.title }}</h3>
+          <p class="hint">{{product.text}}</p>
+          <p class="hint">{{product.time}}</p>
         </div>
       </li>
     }
--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,16 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { TelegramService } from '../../services/telegram.service';
 import { ProductsService } from '../../services/products.service';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
-  template: `<h1>Shop</h1>`
+  imports: [ProductListComponent],
+  template: `
+    <app-product-list
+      title="Отдельный навык"
+      subtitle="Изучите востребованные технологии"
+      [products]="products.byGroup['skill']"
+    />
+    <app-product-list
+      title="Интенсивы"
+      subtitle="Экспресс-программы "
+      [products]="products.byGroup['intensive']"
+    />
+    <app-product-list
+      title="Бесплатные курсы"
+      subtitle="Необходимые навыки и проекты за ваши старания"
+      [products]="products.byGroup['course']"
+    />
+  `
 })
 export class ShopComponent {
 
-  constructor(private telegram: TelegramService, private products: ProductsService) {
+  constructor(public products: ProductsService, public telegram: TelegramService ) {
     this.telegram.MainButton.show();
     console.log(this.products.byGroup)
   }
