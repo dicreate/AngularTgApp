12871b31326d30f91940f4d4053bd55425e4c12d
DmitriyKovchugo <dkovchugo@mail.ru>
2024-01-15 14:26:38 +0300

    feat: add link to product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { ProductComponent } from './pages/product/product.component';
 export const routes: Routes = [
   { path: '', component: ShopComponent, pathMatch: 'full'},
   { path: 'feedback', component: FeedbackComponent },
-  { path: 'product:/:id', component: ProductComponent}
+  { path: 'product/:id', component: ProductComponent}
 ];
